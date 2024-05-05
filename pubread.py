341015f67e50c0fbf4cbbd0cad062eee65173caf
Nurtal <nathan.foulquier.pro@gmail.com>
2024-05-05 17:39:17 +0200

    craft first test function

--- pubread.py
+++ pubread.py
@@ -1,10 +1,29 @@
 import speech_recognition as sr
 
 
-# init recognizer
-recognizer = sr.Recognizer()
+def extract_text_from_audio(audio_file):
+    """Extract text content from audio file
+    work with .wav file
+
+    Args:
+        audio_file (str) : path to the audio file
+
+    Return:
+        (str) : content extracted from audio file
+    
+    """
+    
+    r = sr.Recognizer()
+    data = sr.AudioFile(audio_file)
+    with data as source:
+        audio = r.record(source)
+    text = r.recognize_google(audio)
+    return text
 
 
 
-print("This is Tardis")
+if __name__ == "__main__":
+
+    machin = extract_text_from_audio("data/harvard.wav")
+    print(machin)
 
